ba5f6d51ecc03c5d573e3a976f62727b92ffee72
Aiden Letourneau <aidenletourneau@gmail.com>
2024-03-29 19:10:55 -0400

    starting project

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const ejs = require('ejs')
 app = express();
 
 app.listen(3000);
 
-app.use()
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
